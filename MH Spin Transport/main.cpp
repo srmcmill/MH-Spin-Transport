c244b1e471f40783466912330b54c7ef8bb3d0e7
Stephen McMillan <smcmillan@Stephens-MacBook-Air.local>
2015-01-26 15:54:18 -0600

    Initial Commit

---
+++ MH Spin Transport/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MH Spin Transport
+//
+//  Created by Stephen McMillan on 1/26/15.
+//  Copyright (c) 2015 Stephen McMillan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
